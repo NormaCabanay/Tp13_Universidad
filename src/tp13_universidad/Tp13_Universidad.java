40b6d3529db9496b0d9c4d2e704303b9df90c67c
LuciaYuhasz <luciayuhasz@gmail.com>
2023-10-08 09:44:25 -0300

    Initial commit

---
+++ src/tp13_universidad/Tp13_Universidad.java
@@ -0,0 +1,70 @@
+
+package tp13_universidad;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+
+public class Tp13_Universidad {
+
+    
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+          
+            
+            String URL="jdbc:mariadb://localhost:3306/universidad";
+            String usuario="root";
+            String password="";
+            Connection con= DriverManager.getConnection(URL,usuario,password);
+            
+//            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
+//                    +"values(22548754,'Lopez','Juan','2000-02-14',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"Alumno agregado");}
+           
+
+//     OBTENER LOS DATOS DE LOS ALUMNOS ACTIVOS
+
+        String sql="select * from alumno where estado=true";
+        PreparedStatement ps=con.prepareStatement(sql);
+        ResultSet rs=ps.executeQuery();
+        while(rs.next()){
+        int id=rs.getInt("idAlumno");
+        int dni=rs.getInt("dni");
+        String apellido=rs.getString("apellido");
+        String nombre= rs.getString("nombre");
+        LocalDate fechN=rs.getDate("fechaDeNacimiento").toLocalDate();
+        boolean estado=rs.getBoolean("estado");
+            System.out.println("-----------------------");
+            System.out.println("Dni"+dni);
+            System.out.println("apellido"+apellido);
+            System.out.println("fecha"+fechN.toString());
+        
+        
+        }
+        
+
+
+      
+                   
+           
+       
+                    
+        }catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null,"Error al cargar el Driver");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion");
+            ex.printStackTrace();
+        }
+       
+    }
+    
+}
