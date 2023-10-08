02809d36d2f70a7cfc7677fb5817d2a80b1f5da9
LuciaYuhasz <luciayuhasz@gmail.com>
2023-10-08 10:20:05 -0300

    agregar 4 materias

--- src/tp13_universidad/Tp13_Universidad.java
+++ src/tp13_universidad/Tp13_Universidad.java
@@ -24,8 +24,9 @@ public class Tp13_Universidad {
             
             
             
-//      INGRESAR UN NUEVO ALUMNO  
-
+//    A)  INGRESAR 3 ALUMNOs  
+//        1)
+     
 //            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
 //                    +"values(22548754,'Lopez','Juan','2000-02-14',true)";
 //           PreparedStatement ps= con.prepareStatement(sql);
@@ -34,7 +35,7 @@ public class Tp13_Universidad {
 //           if(filas>0){
 //           JOptionPane.showMessageDialog(null,"Alumno agregado");}
 
-//    INGRESO SEGUNDO ALUMNO
+//       2)
 
 //            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
 //                    +"values(22456879,'Guitierrez','Roberto','1860-08-24',true)";
@@ -43,7 +44,7 @@ public class Tp13_Universidad {
 //          if(filas>0){
 //           JOptionPane.showMessageDialog(null,"Alumno agregado");}
 
-
+//       3)
 //
 //                 String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
 //                   +"values(56988421,'Fonseca','Andrea','1863-12-05',true)";
@@ -52,6 +53,57 @@ public class Tp13_Universidad {
 //           int filas= ps.executeUpdate();
 //           if(filas>0){
 //           JOptionPane.showMessageDialog(null,"Alumno agregado");}
+
+//---------------------------------------------------------------------------------------------------------
+
+//  B)    INSERTAR 4 MATERIAS
+
+
+//      1)  String sql="insert into materia (anio,nombre,estado)"
+//                   +"values(2,'matematica',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"materia agregada");}
+
+//       2
+//            String sql="insert into materia (anio,nombre,estado)"
+//                   +"values(1,'politica',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"materia agregada");}
+
+
+//      3)
+//             String sql="insert into materia (anio,nombre,estado)"
+//                   +"values(3,'geografia',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"materia agregada");}
+
+
+//        4)
+
+//             String sql="insert into materia (anio,nombre,estado)"
+//                   +"values(1,'cienciasSociales',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"materia agregada");}
+
+
+
+
+
+//
+
+
 //           
 
 //     OBTENER LOS DATOS DE LOS ALUMNOS ACTIVOS
