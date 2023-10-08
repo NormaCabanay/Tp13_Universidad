dfb9d348ab180232ca907c73b9ab10119e484cae
LuciaYuhasz <luciayuhasz@gmail.com>
2023-10-08 10:10:02 -0300

    ingreso de alumnos

--- src/tp13_universidad/Tp13_Universidad.java
+++ src/tp13_universidad/Tp13_Universidad.java
@@ -22,6 +22,10 @@ public class Tp13_Universidad {
             String password="";
             Connection con= DriverManager.getConnection(URL,usuario,password);
             
+            
+            
+//      INGRESAR UN NUEVO ALUMNO  
+
 //            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
 //                    +"values(22548754,'Lopez','Juan','2000-02-14',true)";
 //           PreparedStatement ps= con.prepareStatement(sql);
@@ -29,27 +33,46 @@ public class Tp13_Universidad {
 //           int filas= ps.executeUpdate();
 //           if(filas>0){
 //           JOptionPane.showMessageDialog(null,"Alumno agregado");}
-           
+
+//    INGRESO SEGUNDO ALUMNO
+
+//            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
+//                    +"values(22456879,'Guitierrez','Roberto','1860-08-24',true)";
+//           PreparedStatement ps= con.prepareStatement(sql);
+//            int filas= ps.executeUpdate();
+//          if(filas>0){
+//           JOptionPane.showMessageDialog(null,"Alumno agregado");}
+
+
+//
+//                 String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
+//                   +"values(56988421,'Fonseca','Andrea','1863-12-05',true)";
+//          PreparedStatement ps= con.prepareStatement(sql);
+//           
+//           int filas= ps.executeUpdate();
+//           if(filas>0){
+//           JOptionPane.showMessageDialog(null,"Alumno agregado");}
+//           
 
 //     OBTENER LOS DATOS DE LOS ALUMNOS ACTIVOS
 
-        String sql="select * from alumno where estado=true";
-        PreparedStatement ps=con.prepareStatement(sql);
-        ResultSet rs=ps.executeQuery();
-        while(rs.next()){
-        int id=rs.getInt("idAlumno");
-        int dni=rs.getInt("dni");
-        String apellido=rs.getString("apellido");
-        String nombre= rs.getString("nombre");
-        LocalDate fechN=rs.getDate("fechaDeNacimiento").toLocalDate();
-        boolean estado=rs.getBoolean("estado");
-            System.out.println("-----------------------");
-            System.out.println("Dni"+dni);
-            System.out.println("apellido"+apellido);
-            System.out.println("fecha"+fechN.toString());
-        
-        
-        }
+//        String sql="select * from alumno where estado=true";
+//        PreparedStatement ps=con.prepareStatement(sql);
+//        ResultSet rs=ps.executeQuery();
+//        while(rs.next()){
+//        int id=rs.getInt("idAlumno");
+//        int dni=rs.getInt("dni");
+//        String apellido=rs.getString("apellido");
+//        String nombre= rs.getString("nombre");
+//        LocalDate fechN=rs.getDate("fechaDeNacimiento").toLocalDate();
+//        boolean estado=rs.getBoolean("estado");
+//            System.out.println("-----------------------");
+//            System.out.println("Dni"+dni);
+//            System.out.println("apellido"+apellido);
+//            System.out.println("fecha"+fechN.toString());
+//        
+//        
+//        }
         
 
 
